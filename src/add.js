1c5ed24e1849e418c0404e88848e061839eb632d
RatzGabriel <gabrielraz84@gmail.com>
2023-01-13 08:51:52 +0100

    test

--- src/add.js
+++ src/add.js
@@ -1,12 +1,15 @@
 export default add
 
 /**
- * This method will perform addition operation.
+ * Thiss method will perform addition operation.
  *
  * @param {Number} num1 - first number for addition
  * @param {Number} num2 - second  number for additon
  * @return {Number} - Result of addition
  */
 function add(num1, num2) {
-  return num1 + num2
+  const a = num1
+  const be = num2
+
+  return a + be
 }
